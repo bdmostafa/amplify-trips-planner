e7fe6d28554f53a65d3801c9f4eac3178067c53e
mostafa-bs23 <mostafa.mahmud@brainstation-23.com>
2023-01-06 17:28:40 +0600

    Run amplify add storage and push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3e482c4e1": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
